1ac9c890465cd96a1b66385acb4dcd64ddc73700
Ad Friedman <adien330@yahoo.com>
2018-12-25 21:07:46 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,8 +1,8 @@
-# 
+#
 # WIN_COMBINATIONS = [[0,1,2], [3,4,5],[6,7,8], [0,4,8], [2,4,6], [1,4,7],
 # [2,5,8], [0,3,6]]
-# 
-# 
+#
+#
 # def display_board(board=[" ", " ", " ", " ", " ", " "," ", " ", " "])
 # p " #{board[0]} | #{board[1]} | #{board[2]} "
 # p "-----------"
@@ -10,15 +10,15 @@
 # p "-----------"
 # p " #{board[6]} | #{board[7]} | #{board[8]} "
 # end
-# 
+#
 # def input_to_index(user_input)
 #   user_input.to_i - 1
 # end
-# 
+#
 # def move(board, index, character)
 # board[index] = character
 # end
-# 
+#
 # def position_taken?(board,index)
 #   if board[index] == " "
 #     return false
@@ -26,16 +26,16 @@
 #     return true
 #       end
 # end
-# 
-# 
+#
+#
 # def valid_move?(board,index)
 #   if position_taken?(board,index) == false and index >= 0 and index <= 8
 #     return true
 #   end
 # return false
 # end
-# 
-# 
+#
+#
 # def turn(board)
 #   puts "Please enter a number 1-9: "
 #   user_input = gets.strip
@@ -47,14 +47,14 @@
 #   else
 #     turn(board)
 #   end
-# 
+#
 # end
-# 
-# 
+#
+#
 # def turn_count(board)
 #   board.count{|i| i =="X" or i == "O"}
 # end
-# 
+#
 # def current_player(board)
 #   turn = turn_count(board)
 #   if turn%2 == 0
@@ -63,8 +63,8 @@
 #     return "O"
 #   end
 # end
-# 
-# 
+#
+#
 # def won?(board)
 #   WIN_COMBINATIONS.each do |i|
 #     if (board[i[0]] == "X" and board[i[1]] == "X" and board[i[2]] == "X")
@@ -73,19 +73,19 @@
 #       return i
 #     end
 #   end
-# 
+#
 #   return false
 # end
-# 
-# 
-# 
+#
+#
+#
 # # def full?(board)
 # #   if turn_count(board) == 9
 # #     return true
 # #   end
 # #   return false
 # # end
-# 
+#
 # def full?(board)
 #    if board.detect {|i| i == " " || i == nil}
 #      return false
@@ -93,8 +93,8 @@
 #      return true
 #    end
 #  end
-# 
-# 
+#
+#
 # def draw?(board)
 # if !won?(board) and full?(board)
 #   return true
@@ -102,7 +102,7 @@
 #   return false
 # end
 # end
-# 
+#
 # def over?(board)
 #   if draw?(board)
 #     return true
@@ -114,7 +114,7 @@
 #   return false
 # end
 # end
-# 
+#
 # def winner(board)
 #   if won?(board)
 #     if board.count{|i| i=="X"}%2 == 0
@@ -123,16 +123,16 @@
 #       return "X"
 #     end
 #   end
-# 
+#
 # end
-# 
-# 
-# 
+#
+#
+#
 # def play(board)
 #    until over?(board) == true
 #      turn(board)
 #    end
-# 
+#
 #    if won?(board) !=false
 #      puts "Congratulations #{winner(board)}!"
 #    elsif draw?(board)
@@ -140,8 +140,8 @@
 #    else
 #      puts "Congratulations #{current_player(board)}!"
 #    end
-# 
-# 
+#
+#
 #  end
 # Helper Method
 def position_taken?(board, index)
